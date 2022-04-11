7a288190aa44524ffecde519f0418ed2de159186
Rafael Dias <rafaeldsd93@gmail.com>
2022-04-11 21:26:09 +0100

    fileManager

---
+++ fileManager/src/test/java/com/egs/fileManager/FileManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egs.fileManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
