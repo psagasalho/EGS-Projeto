496e1405b467114010ba6321bf6b9d824a8f3ab0
luciasousa <luciamsousa00@ua.pt>
2022-05-03 23:41:37 +0100

    payment service adjustments

--- Payment/payment.py
+++ Payment/payment.py
@@ -22,91 +22,66 @@ class methodsList(db.Model):
 
 #database table - transactions
 class transaction(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    transaction_id = db.Column(db.String(120), primary_key=True)
+    order_id = db.Column(db.Integer, nullable=False)
     user_id = db.Column(db.String(80), nullable=False)
+    method_id = db.Column(db.Integer, nullable=False)
     amount = db.Column(db.String(80),nullable=False)
     successful = db.Column(db.String(80),nullable=False)
 
 class Wallet:
     user = ""
     amount = 0
-    def __init__(self, username,amount):
+    def __init__(self, username, amount):
         self.user = username
         self.amount = amount
     
     def registerWallet(self):
-        return {}
+        return (jsonify({"user" : self.user},{"amount" : self.amount}))
 
     def performPayment(self,price):
-        return self.amount-price
+        self.amount=self.amount-price
+        return self.amount
 
 class MethodForm(FlaskForm):
     type = StringField(validators=[InputRequired(),Length(
         min=4, max=20)], render_kw={"placeholder" : "Username"})
 
-#método de pagamento
-@app.route('/payment/<int:product_id>', methods=["GET", "POST"] )
-def method(product_id):
-    methodList=db.Query("SELECT * FROM methodsList order by method")
-    return render_template("method.html",templates=methodList )
-    #methods = ['Credit', 'Debit', 'MB']
-    #return render_template('method.html', templates=methods)
-    #form = MethodForm()
-    #if form.validate_on_submit():
-    #    return redirect(url_for('dashboard')) 
-    #return render_template ('method.html', form = form)
+#selects payment method and get payment information
+@app.route('/payment/<int:order_id>', methods=["GET", "POST"] )
+def method(order_id, method=None, price=None, username=None):
 
-@app.route('/payment/<product_id>/<method_id>/<token>', methods=["POST"])
-def transact(product_id,method_id,token):
-    
-    #name = request.json.get('token')
-    name = token
-    price = product_id
-    #price = request.json.get('product_id')
-    amount = 1000
-    wallet = Wallet(name, amount)
-    #user id = token
-    #token = request.json.get('token')
+    return (jsonify({"order_id" : order_id},{"method" : method},{"price" : price}, {"username" : username}))
 
-    if price.isdigit() == False :
+#confirm payment
+@app.route('/payment/<int:order_id>/<int:method_id>', methods=["POST"])
+def transact(order_id,method_id,username=None,nif=None,price=None):
+    #transaction id is the primary key of the database, and is the concatenation of the order id and the username
+    transaction_id=str(order_id+username)
+    if nif.isdigit() == False :
         return (jsonify({
-            "message" : "invalid amount"
+            "message" : "invalid NIF"
         }))
-    #elif len(name) != 12:
-    #    return (jsonify({
-    #        "message" : "invalid username"
-    #    }))
-    elif not token:
-        return (jsonify({"message": "invalid user"}))
+    elif len(nif) != 9:
+        return (jsonify({
+            "message" : "invalid NIF"
+        }))
     else:
-        new_transaction = transaction(user_id=token, successful="no", amount=price)
+        new_transaction = transaction(transaction_id=transaction_id,order_id=order_id, user_id=username, method_id=method_id, successful="no", amount=price)
         db.session.add(new_transaction)
         db.session.commit()
+        return (jsonify({ "message" : "confirm payment"}))
 
-        return "complete transaction"#url for "complete_trasaction"
-
-#complete transaction
-@app.route('/payment/<string:token>', methods=["POST"])
-def complete_trasaction(token):
-    #otp =  request.json.get('otp')
-    #if user proceeds to pay
-    otp=TRUE
-    if otp:
-        successful = transaction.query.filter_by(user_id=token).update(dict(successful= "yes"))
+#complete transaction and update payment state
+@app.route('/payment/<transaction_id>', methods=["POST"])
+def complete_trasaction(username, transaction_id):
+    #TODO: check if user is valid
+    if username is True:
+        successful = transaction.query.filter_by(transaction_id=transaction_id).update(dict(successful= "yes"))
         db.session.commit()
         return  (jsonify({"message": "transaction successful"}))
     else:
         return (jsonify({"message": "transaction not successful"}))
-
-#atualizar estado do pagamento
-#@app.route('/payment/<int:product_id>/<string:token>/state', methods=['PUT'])
-#def put(product_id):
-#    return 0
-
-#retornar informação
-@app.route('/payment/<int:product_id>/<string:token>', methods=['GET'])
-def get(product_id,token):
-    return {"product" :product_id}
 
 if __name__ == "__main__":
     app.run(debug=True)
