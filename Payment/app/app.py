4d215b5bf10568769797136e6508232329eef801
Sérgio Gasalho <paulogasalho@ua.pt>
2022-05-11 15:04:13 +0100

    Docker done in Payment

--- Payment/app/app.py
+++ Payment/app/app.py
@@ -12,7 +12,7 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 db = SQLAlchemy(app)
 
 #database table - transactions
-class transaction(db.Model):
+class Transaction(db.Model):
     transaction_id = db.Column(db.String(120), primary_key=True)
     order_id = db.Column(db.Integer, nullable=False)
     method_id = db.Column(db.Integer, nullable=False)
@@ -20,23 +20,16 @@ class transaction(db.Model):
     amount = db.Column(db.String(80),nullable=False)
     successful = db.Column(db.String(80),nullable=False)
 
-#selects payment method and get payment information
-@app.route('/payment/<int:order_id>', methods=["GET", "POST"] )
+@app.route('/createDB')
+def createDatabase():
+  db.create_all()
+  return jsonify({'message':'Success'})
+
+#method to obtain order payment information
+@app.route('/payment/<int:order_id>', methods=["GET"] )
 def method(order_id):
-    if 'method' not in request.values:
-        return jsonify("Bad Request"),400
-    if 'price' not in request.values:
-        return jsonify("Bad Request"),400
-    if 'username' not in request.values:
-        return jsonify("Bad Request"),400
-
-    method=request.values['method']
-    price=request.values['price']
-    username=request.values['username']
-    if None not in (method,price,username):
-        result = (jsonify({"order_id" : order_id},{"method" : method},{"price" : price},{"username" : username}))
-
-    return result
+    transaction = Transaction.query.filter_by(order_id=order_id).first() 
+    return jsonify({"order_id" : transaction.order_id},{"method" : transaction.method_id},{"price" : transaction.amount},{"username" : transaction.user_id})
 
 #confirm payment
 @app.route('/payment/<int:order_id>/<int:method_id>', methods=["POST"])
@@ -45,26 +38,15 @@ def transact(order_id, method_id):
         return jsonify("Bad Request"),400
     if 'username' not in request.values:
         return jsonify("Bad Request"),400
-    if 'nif' not in request.values:
-        return jsonify("Bad Request"),400
     price=request.values['price']
     username=request.values['username']
-    nif=request.values['nif']
     #transaction id is the primary key of the database, and is the concatenation of the order id and the username
     transaction_id=str(str(order_id)+username)
-    if nif.isdigit() == False :
-        return (jsonify({
-            "message" : "invalid NIF"
-        }))
-    elif len(nif) != 9:
-        return (jsonify({
-            "message" : "invalid NIF"
-        }))
-    else:
-        new_transaction = transaction(transaction_id=transaction_id,order_id=order_id,method_id=method_id, user_id=username, successful="no", amount=price)
-        db.session.add(new_transaction)
-        db.session.commit()
-        return (jsonify({ "message" : "confirm payment"}))
+    
+    new_transaction = Transaction(transaction_id=transaction_id,order_id=order_id,method_id=method_id, user_id=username, successful="no", amount=price)
+    db.session.add(new_transaction)
+    db.session.commit()
+    return jsonify({ "message" : "Confirm Payment"})
 
 #complete transaction and update payment state
 @app.route('/payment/<string:token>', methods=["POST"])
@@ -77,11 +59,11 @@ def complete_trasaction(token):
     username=request.values['username']
     transaction_id=str(str(order_id)+username)
     if token is not None:
-        successful = transaction.query.filter_by(transaction_id=transaction_id).update(dict(successful= "yes"))
+        successful = Transaction.query.filter_by(transaction_id=transaction_id).update(dict(successful= "yes"))
         db.session.commit()
-        return  (jsonify({"message": "transaction successful"}))
+        return  (jsonify({"message": "Transaction Successful"}))
     else:
-        return (jsonify({"message": "transaction not successful"}))
+        return (jsonify({"message": "Transaction not Successful"}))
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
